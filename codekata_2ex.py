34e4af0f444afd257467f82588407c0dafb5af45
ssvdata87 <112293041+ssvdata87@users.noreply.github.com>
2022-09-29 11:45:05 +0530

    codekata_exaples_problem
    codekata_exaples_problem

---
+++ codekata_2ex.py
@@ -0,0 +1,27 @@
+alist = []
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+alist.append(a)
+alist.append(b)
+alist.append(c)
+
+print(max(alist))
+
+'''You are given three numbers A, B & C. Print the largest amongst these three numbers.
+
+Input Description:
+Three numbers are provided to you.
+
+Output Description:
+Find and print the largest among the three
+
+Sample Input :
+1
+2
+3
+Sample Output :
+3'''
